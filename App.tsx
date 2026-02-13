147c63b004240841be17d345d4cc687ae4d23cac
CK <mutembei.kaburu@gmail.com>
2026-02-13 17:56:25 +0100

    Init SQLite on app startup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { sqliteRepo } from './src/db/sqliteRepo';
+
 export default function App() {
+  useEffect(() => {
+    sqliteRepo.init().catch((error) => {
+      console.error('Failed to initialize SQLite repository', error);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Flowcycle Spike</Text>
       <StatusBar style="auto" />
     </View>
   );
